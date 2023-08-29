4daf0c783352385fe2e58bf91f1f86f0aaab56f6
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-08-30 01:50:33 +0530

    Fixed some bugs

--- client/src/screens/UserAdminAuth.jsx
+++ client/src/screens/UserAdminAuth.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import {showInfoToast,showErrorToast} from 'toast';
+import { showInfoToast, showErrorToast } from 'toast';
 import CtaButton from "../components/CtaButton";
-import {showLoader,hideLoader} from "utility"
+import { showLoader, hideLoader } from "utility"
 
 export default function UserAdminAuth() {
   const [showname, setShowname] = useState(false)
@@ -12,30 +12,30 @@ export default function UserAdminAuth() {
   const [password, setPassword] = useState("")
   const history = useHistory()
 
-  function handleForgot(event){
+  function handleForgot(event) {
     event.preventDefault()
     showLoader()
     let element = document.querySelector('#useradminauth div.backdrop form fieldset input[type="email"]')
-    if(element.checkValidity()){
-      fetch(`${process.env.REACT_APP_BACKEND}/user/forgot-password`,{
-        method:"post",
-        headers:{
-          "Content-Type":"application/json"
+    if (element.checkValidity()) {
+      fetch(`${process.env.REACT_APP_BACKEND}/user/forgot-password`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
         },
-        body:JSON.stringify({
+        body: JSON.stringify({
           email,
         })
       })
-      .then(res=>res.json())
-      .then(data => {
-        hideLoader()
-        if(data.message)
-        showInfoToast(data.message)
-        else showErrorToast(data.error)
-      })
+        .then(res => res.json())
+        .then(data => {
+          hideLoader()
+          if (data.message)
+            showInfoToast(data.message)
+          else showErrorToast(data.error)
+        })
     }
     else
-    element.reportValidity()
+      element.reportValidity()
   }
 
   function authenticate_btn_handler(event) {
@@ -53,18 +53,41 @@ export default function UserAdminAuth() {
           password: password
         })
       })
-      .then(res => res.json()) 
-      .then(data => {
-        hideLoader()
-        console.log(data.error)
-        if(data.error)
-        showErrorToast(data.error.toString())
-        else
-        showInfoToast("Now login with credentials")
-      })
+        .then(res => res.json())
+        .then(data => {
+          hideLoader()
+          console.log(data.error)
+          if (data.error)
+            showErrorToast(data.error.toString())
+          else
+            showInfoToast("Now login with credentials")
+        })
+    }
+    else if (isAdmin) {
+      console.log(email, password)
+      fetch(`${process.env.REACT_APP_BACKEND}/admin`,
+        {
+          method: "post", headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: email,
+            password: password
+          })
+        }).then(res => res.json())
+        .then(data => {
+          hideLoader()
+          if (data.error)
+            showErrorToast(data.error)
+          localStorage.setItem("userId", data.userId)
+          history.push('/admin/addProduct')
+        })
+        .catch(err => {
+          hideLoader()
+          showErrorToast(err)
+        })
     }
     else {
-      fetch(`${process.env.REACT_APP_BACKEND}/signin`, { method: "post", headers: { "Content-Type": "application/json" },
+      fetch(`${process.env.REACT_APP_BACKEND}/signin`, {
+        method: "post", headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           email: email,
           password: password
@@ -85,9 +108,9 @@ export default function UserAdminAuth() {
     }
   }
 
-	let formcomp = () => {
-		if (showname) {
-			return (
+  let formcomp = () => {
+    if (showname) {
+      return (
         <fieldset>
           <input
             type="text"
@@ -101,17 +124,17 @@ export default function UserAdminAuth() {
           <label htmlFor="name">Name</label>
         </fieldset>
       )
-		}
-	}
+    }
+  }
 
-	return (
-		<section id="useradminauth" >
-			<img className="backimage" src="/assets/images/backimage.jpg" alt="background" />
-			<div className="backdrop container-fluid" >
-        <div className="row w-100" style={{marginRight:"0"}}>
+  return (
+    <section id="useradminauth" >
+      <img className="backimage" src="/assets/images/backimage.jpg" alt="background" />
+      <div className="backdrop container-fluid" >
+        <div className="row w-100" style={{ marginRight: "0" }}>
           <div className="offset-lg-3 col-lg-6 col-xs-12">
 
-            <div className="row" style={{marginLeft:"auto",marginRight:"auto"}}>
+            <div className="row" style={{ marginLeft: "auto", marginRight: "auto" }}>
               <div className="col-12 heading">
                 {isAdmin && !showname ? <h1 className="brand">Hello Admin</h1> : <h1 className='brand'><Link to="/"> CovidMart</Link></h1>}
                 <h4 className="title" >
@@ -120,46 +143,46 @@ export default function UserAdminAuth() {
               </div>
               <div className="col-12 body">
                 <form className="myform" onSubmit={authenticate_btn_handler} autoComplete="off">
-                {formcomp()}
-                <fieldset>
-                  <input
-                    type="email"
-                    autoComplete="off"
-                    placeholder="Email"
-                    id="email"
-                    name="email"
-                    title="Please an enter a valid email address"
-                    onChange={(e) => { setEmail(e.target.value) }}
-                    required
-                  />
-                  <label htmlFor="email">Email</label>
-                </fieldset>
-                <fieldset>
-                  <input
-                    placeholder="Password"
-                    autoComplete="off"
-                    type="password"
-                    name="password"
-                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                    title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters."
-                    id="password"
-                    onChange={(e) => { setPassword(e.target.value) }}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </fieldset>
-                <div className="row auth">
-                  <div className="col-12 row justify-content-between">
-                    <CtaButton
-                      solid
-                      type="submit"
-                    >
-                      <span className={`fa-solid ${showname?"fa-user-plus":"fa-arrow-right-to-bracket"} icon`}></span>
-                      <span className={`text`}>{showname?"Signup":"Signin"}</span>
-                    </CtaButton>
-                    <button onClick={handleForgot}>{!showname?"Forgot Password ?":""}</button>
+                  {formcomp()}
+                  <fieldset>
+                    <input
+                      type="email"
+                      autoComplete="off"
+                      placeholder="Email"
+                      id="email"
+                      name="email"
+                      title="Please an enter a valid email address"
+                      onChange={(e) => { setEmail(e.target.value) }}
+                      required
+                    />
+                    <label htmlFor="email">Email</label>
+                  </fieldset>
+                  <fieldset>
+                    <input
+                      placeholder="Password"
+                      autoComplete="off"
+                      type="password"
+                      name="password"
+                      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                      title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters."
+                      id="password"
+                      onChange={(e) => { setPassword(e.target.value) }}
+                      required
+                    />
+                    <label htmlFor="password">Password</label>
+                  </fieldset>
+                  <div className="row auth">
+                    <div className="col-12 row justify-content-between">
+                      <CtaButton
+                        solid
+                        type="submit"
+                      >
+                        <span className={`fa-solid ${showname ? "fa-user-plus" : "fa-arrow-right-to-bracket"} icon`}></span>
+                        <span className={`text`}>{showname ? "Signup" : "Signin"}</span>
+                      </CtaButton>
+                      <button onClick={handleForgot}>{!showname ? "Forgot Password ?" : ""}</button>
+                    </div>
                   </div>
-                </div>
                 </form>
                 <div>
                   <button onClick={() => { setIsAdmin(true) }} >
@@ -168,11 +191,11 @@ export default function UserAdminAuth() {
                 </div>
                 <div>
                   <button onClick={() => {
-                        if(isAdmin) setIsAdmin(false)
-                        else setShowname(prevstate => !prevstate);
-                      }}
+                    if (isAdmin) setIsAdmin(false)
+                    else setShowname(prevstate => !prevstate);
+                  }}
                   >
-                    {showname || isAdmin? "Back" : "Create account"}
+                    {showname || isAdmin ? "Back" : "Create account"}
                   </button>
                 </div>
               </div>
@@ -180,7 +203,7 @@ export default function UserAdminAuth() {
 
           </div>
         </div>
-			</div>
-		</section>
-	);
+      </div>
+    </section>
+  );
 }
